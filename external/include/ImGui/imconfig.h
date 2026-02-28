4dfe20e089a460091275e1ae2f19ddaee6736e1e
VadoF1 <vadof1.vf1@gmail.com>
2026-02-28 00:11:17 -0600

    Set Up Part 1

--- external/include/ImGui/imconfig.h
+++
@@ -1,7 +0,0 @@
-// Minimal imconfig.h to satisfy ImGui include when not using a custom config.
-#ifndef IMCONFIG_H
-#define IMCONFIG_H
-
-// Leave empty - add configuration macros here if needed.
-
-#endif // IMCONFIG_H
